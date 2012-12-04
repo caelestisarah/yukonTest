2018b5ac5827acd822e9b48b0a71c24f686c40a3
Sarah Gilliland <caelestisarah@gmail.com>
2012-12-03 20:42:23 -0600

    Finished script, testing on commit

--- TestProg.cpp
+++ TestProg.cpp
@@ -7,16 +7,23 @@ using namespace std;
 
 int overloaded(int i)
 {
+	//I am modifying every single function
     return i + 1;
+	//in multiple places!
 }
 
 int overloaded(int i, int j)
 {
+	i = 0;
+	i = 10;
+	j = i;
+	j++;
     return i + j;
 }
 
 int test2(int i, int j)
 {
+	j*=41;
 	//This is another very important function!
 	i = i + 10;
     return i + j;
@@ -27,10 +34,12 @@ int main(int argc, char** argv)
 	//This is a very important function!!
     int first = 1;
     int second = 2;
-
+	//I'm doing some more changes now
+	int newInt = test2(1,2);
     int third = overloaded(first);
     int fourth = overloaded(third, second);
     int fifth = test2(first, second);
+	int somenewdude = 328954329;
 
 	//I don't think these should be here
     cout << "first: " << first << endl;
